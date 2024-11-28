66796a89312b76c584764277e9a949c0d0b136e8
douglas <douglasmartinssantos7@gmail.com>
2024-11-28 13:06:17 -0300

    new fixtures

--- src/test/kotlin/br/com/vraptor/forum/fixtures/CursoTest.kt
+++ src/test/kotlin/br/com/vraptor/forum/fixtures/CursoTest.kt
@@ -1,4 +1,9 @@
 package br.com.vraptor.forum.fixtures
 
 object CursoTest {
+    fun build() = Curso(
+        id = 1,
+        nome = "Aprendendo Kotlin",
+        categoria = "Programação"
+    )
 }
--- src/test/kotlin/br/com/vraptor/forum/fixtures/UsuarioTest.kt
+++ src/test/kotlin/br/com/vraptor/forum/fixtures/UsuarioTest.kt
@@ -1,4 +1,11 @@
 package br.com.vraptor.forum.fixtures
 
 object UsuarioTest {
+    fun build() = Usuario(
+        id = 1,
+        nome = "Brunelli",
+        email = "brunelli@email.com",
+        password = "123456",
+        role = mutableListOf(Role(id = 1, nome = "READ_WRITE"))
+    )
 }
